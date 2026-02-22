01d41ec37f3ba17084dde4c45c14622f820fe5a4
LTxai <lfonceca@hubees.com.br>
2026-02-22 14:30:50 -0300

    refactor band card

--- frontend/discografias-web/src/app/app.ts
+++ frontend/discografias-web/src/app/app.ts
@@ -1,26 +1,12 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Card } from './features/bands/components/card/card';
-import { BandsService } from './core/services/bands';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Card],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('discografias-web');
-  readonly bandsService = inject(BandsService);
-
-  constructor() {
-    this.bandsService.getBands().subscribe({
-      next: (bands) => {
-        console.log('Bands fetched successfully:', bands);
-      },
-      error: (error) => {
-        console.error('Error fetching bands:', error);
-      },
-    });
-  }
 }
--- frontend/discografias-web/src/app/features/bands/components/card/card.ts
+++ frontend/discografias-web/src/app/features/bands/components/card/card.ts
@@ -1,4 +1,5 @@
 import { Component, input } from '@angular/core';
+import { Band } from '../../../../core/models/band.model';
 
 @Component({
   selector: 'app-card',
@@ -7,6 +8,16 @@ import { Component, input } from '@angular/core';
   styleUrl: './card.scss',
 })
 export class Card {
-  title = input<string>('');
   variant = input<'primary' | 'secondary'>('primary');
+
+  title = input<string>('');
+
+  band = input<Band>({
+    name: '',
+    nationality: '',
+    genre: '',
+    studioAlbumsCount: 0,
+    songsCount: 0,
+    score: 0,
+  });
 }
